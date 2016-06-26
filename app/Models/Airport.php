27570c98b2eab484dc7ba043bb5bb2b94b4b40b8
Muhammad Raza <razachohan@gmail.com>
2016-06-27 04:11:55 +0500

    Add models for database classes
     Changes to be committed:
    	new file:   app/Models/Airport.php
    	new file:   app/Models/AirportExperience.php
    	new file:   app/Models/Review.php
    	new file:   app/Models/TravellerType.php

---
+++ app/Models/Airport.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chohan
+ * Date: 6/27/16
+ * Time: 12:56 AM
+ */
+
+namespace App\Models;
+
+
+class Airport {
+
+}
---
+++ app/Models/TravellerType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chohan
+ * Date: 6/26/16
+ * Time: 11:37 PM
+ */
+
+namespace App\Models;
+
+
+class TravellerType {
+
+}
